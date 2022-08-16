992046da878ae9bb39abcf89352aaedc363009ec
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-08-16 21:56:33 +0000

    Add members to the teams page

---
+++ assets/js/members.js
@@ -0,0 +1,29 @@
+const members = 
+[
+    {
+        id:1,
+        name:'Jagadish',
+        email: 'jagadish_june17'
+    },
+    {
+        id:2,
+        name:'Jagadish2',
+        email: 'Joinjiji'
+    }
+]
+
+
+window.addEventListener("load", function(event) {
+    let team = document.getElementById("players-list");
+
+    for(let i =0; i < members.length; i++) {
+        let row = document.createElement("tr");
+        let cell1 = document.createElement("td");
+        cell1.innerHTML = members[i].name;
+        row.appendChild(cell1);
+        let cell2 = document.createElement("td");
+        cell2.innerHTML = members[i].email;
+        row.appendChild(cell2);
+        team.appendChild(row);
+    }
+}) 
