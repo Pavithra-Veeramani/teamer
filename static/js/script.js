73f7fafa3534be8019f2bf3417c4c11e4f53aa97
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-09-11 22:02:57 +0000

    Add jquery to change font color in header

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,9 @@
 
+$(document).ready(function(){
+
+    $(".nav-item .nav-link").on("click", function(){
+        $(".nav").find(".active").removeClass("active");
+        $(this).addClass("active");
+     });
+
+});
