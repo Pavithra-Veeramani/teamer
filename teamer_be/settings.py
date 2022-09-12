10c4e0494b7c9b9bc743cbbb208309b86c966b42
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-09-12 13:58:23 +0000

    removed secret key

--- teamer_be/settings.py
+++ teamer_be/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@h2@1n81)ec%9u=7swznk8(qju)l15oif8du6^)s&54il0velg')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
